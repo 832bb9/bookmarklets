540d424700f8cbcf46ed76b793dd3f3820c392ff
Ivan Ivanov <31843788+832bb9@users.noreply.github.com>
2020-12-11 17:19:49 +0200

    -

--- src/github-auto-merge.js
+++ src/github-auto-merge.js
@@ -71,147 +71,162 @@ const createApp = (renderApp, initialState = {}) => {
   return unmount;
 };
 
-const selectUpdateBranchBtn = () => {
-  return Array.from(document.querySelectorAll("button")).find(
-    (node) => node.innerText === "Update branch"
-  );
-};
+const main = () => {
+  if (window[`APP:${APP_ID}`]) {
+    console.log(`APP:${APP_ID} already running`);
+    return;
+  }
 
-const selectSquashAndMergeBtn = () => {
-  return Array.from(document.querySelectorAll("button")).find(
-    (node) => node.innerText === "Squash and merge"
-  );
-};
+  window[`APP:${APP_ID}`] = {};
 
-const selectConfirmSquashAndMergeBtn = () => {
-  return Array.from(document.querySelectorAll("button")).find(
-    (node) => node.innerText === "Confirm squash and merge"
-  );
-};
+  const selectUpdateBranchBtn = () => {
+    return Array.from(document.querySelectorAll("button")).find(
+      (node) => node.innerText === "Update branch"
+    );
+  };
 
-const getPullRequestDescriptionText = () => {
-  const innerText = document.querySelector(
-    "#discussion_bucket .js-discussion:first-child .comment-body"
-  ).innerText;
-  return innerText === INITIAL_DESCRIPTION_TEXT ? "" : innerText;
-};
+  const selectSquashAndMergeBtn = () => {
+    return Array.from(document.querySelectorAll("button")).find(
+      (node) => node.innerText === "Squash and merge"
+    );
+  };
 
-const getPullRequestTitleText = () => {
-  return document.querySelector("#partial-discussion-header .js-issue-title")
-    .innerText;
-};
+  const selectConfirmSquashAndMergeBtn = () => {
+    return Array.from(document.querySelectorAll("button")).find(
+      (node) => node.innerText === "Confirm squash and merge"
+    );
+  };
 
-const selectSquashAndMergeDescriptionTextarea = () => {
-  return document.querySelector("#merge_message_field");
-};
+  const getPullRequestDescriptionText = () => {
+    const innerText = document.querySelector(
+      "#discussion_bucket .js-discussion:first-child .comment-body"
+    ).innerText;
+    return innerText === INITIAL_DESCRIPTION_TEXT ? "" : innerText;
+  };
 
-const selectSquashAndMergeTitleInput = () => {
-  return document.querySelector("#merge_title_field");
-};
+  const getPullRequestTitleText = () => {
+    return document.querySelector("#partial-discussion-header .js-issue-title")
+      .innerText;
+  };
 
-const startInterval = () => {
-  return setInterval(() => {
-    console.log("INTERVAL TICK");
+  const selectSquashAndMergeDescriptionTextarea = () => {
+    return document.querySelector("#merge_message_field");
+  };
 
-    const updateBranchBtn = selectUpdateBranchBtn();
+  const selectSquashAndMergeTitleInput = () => {
+    return document.querySelector("#merge_title_field");
+  };
 
-    if (updateBranchBtn) {
-      updateBranchBtn.click();
-      return;
-    }
+  const startInterval = () => {
+    return setInterval(() => {
+      console.log("INTERVAL TICK");
 
-    const squashAndMergeBtn = selectSquashAndMergeBtn();
+      const updateBranchBtn = selectUpdateBranchBtn();
 
-    if (squashAndMergeBtn) {
-      squashAndMergeBtn.click();
-      return;
-    }
-
-    const confirmSquashAndMergeBtn = selectConfirmSquashAndMergeBtn();
-    const pullRequestDescriptionText = getPullRequestDescriptionText();
-    const pullRequestTitleText = getPullRequestTitleText();
-
-    if (confirmSquashAndMergeBtn) {
-      if (pullRequestDescriptionText) {
-        selectSquashAndMergeDescriptionTextarea().value = pullRequestDescriptionText;
-      }
-
-      if (pullRequestTitleText) {
-        selectSquashAndMergeTitleInput().value = pullRequestTitleText;
-      }
-
-      confirmSquashAndMergeBtn.click();
-      return;
-    }
-  }, INTERVAL_TIME);
-};
-
-let intervalId = null;
-let autostart = true;
-
-createApp(
-  ({ state, update, after }) => {
-    after(() => {
-      if (!autostart) {
+      if (updateBranchBtn) {
+        updateBranchBtn.click();
         return;
       }
 
-      autostart = false;
+      const squashAndMergeBtn = selectSquashAndMergeBtn();
 
-      intervalId = startInterval();
+      if (squashAndMergeBtn) {
+        squashAndMergeBtn.click();
+        return;
+      }
 
-      update((current) => ({
-        ...current,
-        interval: {
-          ...current.interval,
-          status: "pending",
-        },
-      }));
-    });
+      const confirmSquashAndMergeBtn = selectConfirmSquashAndMergeBtn();
+      const pullRequestDescriptionText = getPullRequestDescriptionText();
+      const pullRequestTitleText = getPullRequestTitleText();
 
-    after(() => {
-      const onToggleInterval = () => {
-        if (state.interval.status === "idle") {
-          intervalId = startInterval();
-
-          update((current) => ({
-            ...current,
-            interval: {
-              ...current.interval,
-              status: "pending",
-            },
-          }));
-        } else if (state.interval.status === "pending") {
-          clearInterval(intervalId);
-
-          update((current) => ({
-            ...current,
-            interval: {
-              ...current.interval,
-              status: "idle",
-            },
-          }));
+      if (confirmSquashAndMergeBtn) {
+        if (pullRequestDescriptionText) {
+          selectSquashAndMergeDescriptionTextarea().value = pullRequestDescriptionText;
         }
-      };
-    });
 
-    return `
-    <div style="position: fixed; right: 8px; bottom: 8px; padding: 8px; border-radius: 4px; border: 1px solid #000; background-color: #fff; color: #000">
-      <div style="margin-bottom: 8px;">
-        <button data-ref="toggle-interval" style="width: 100%;">Interval is "${state.interval.status}"</button>
+        if (pullRequestTitleText) {
+          selectSquashAndMergeTitleInput().value = pullRequestTitleText;
+        }
+
+        confirmSquashAndMergeBtn.click();
+        return;
+      }
+    }, INTERVAL_TIME);
+  };
+
+  let intervalId = null;
+  let autostart = true;
+
+  createApp(
+    ({ root, state, update, after }) => {
+      after(() => {
+        if (!autostart) {
+          return;
+        }
+
+        autostart = false;
+
+        intervalId = startInterval();
+
+        update((current) => ({
+          ...current,
+          interval: {
+            ...current.interval,
+            status: "pending",
+          },
+        }));
+      });
+
+      after(() => {
+        const onToggleInterval = () => {
+          if (state.interval.status === "idle") {
+            intervalId = startInterval();
+
+            update((current) => ({
+              ...current,
+              interval: {
+                ...current.interval,
+                status: "pending",
+              },
+            }));
+          } else if (state.interval.status === "pending") {
+            clearInterval(intervalId);
+
+            update((current) => ({
+              ...current,
+              interval: {
+                ...current.interval,
+                status: "idle",
+              },
+            }));
+          }
+        };
+
+        root
+          .querySelector('[data-ref="toggle-interval"]')
+          .addEventListener("click", onToggleInterval);
+      });
+
+      return `
+      <div style="position: fixed; right: 8px; bottom: 8px; padding: 8px; border-radius: 4px; border: 1px solid #000; background-color: #fff; color: #000">
+        <div style="margin-bottom: 8px;">
+          <button data-ref="toggle-interval" style="width: 100%;">Interval is "${state.interval.status}"</button>
+        </div>
+        <div>
+          CHECKING UPDATES: ${state.updates.status}
+        </div>
       </div>
-      <div>
-        CHECKING UPDATES: ${state.updates.status}
-      </div>
-    </div>
-  `;
-  },
-  {
-    interval: {
-      status: "idle",
+    `;
     },
-    updates: {
-      status: "idle",
-    },
-  }
-);
+    {
+      interval: {
+        status: "idle",
+      },
+      updates: {
+        status: "idle",
+      },
+    }
+  );
+};
+
+main();
